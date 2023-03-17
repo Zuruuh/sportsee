996d9b614ef2289b06f9380fdd1999a7f13c407f
Zuruuh <ziadi.mail.pro@gmail.com>
2023-03-17 13:27:25 +0100

    🔧 Add colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
   content: ['./index.html', 'src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#E60000',
+      secondary: '#020203',
+      'text-primary': '#000000',
+      'text-secondary': '#FFFFFF',
+      'bg-primary': '#FFFFFF',
+    },
   },
   plugins: [],
 };
