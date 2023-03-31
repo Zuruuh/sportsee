8792de8dc4cfe29e4d35676c457e253ea3f0e81b
Zuruuh <ziadi.mail.pro@gmail.com>
2023-03-31 15:41:42 +0200

    ✨ Fix ladle story for Square Icon component

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,8 +1,1 @@
-// import type { GlobalProvider } from '@ladle/react';
 import '../src/shared/styles/index.css';
-
-// export const Provider: GlobalProvider = ({
-//   children,
-//   globalState,
-//   storyMeta,
-// }) => <>{children}</>;
--- src/shared/components/SquareIcon/SquareIcon.stories.tsx
+++ src/shared/components/SquareIcon/SquareIcon.stories.tsx
@@ -1,15 +1,34 @@
-import type { StoryDefault } from '@ladle/react';
-import {
+import type { StoryDefault, Story } from '@ladle/react';
+import SquareIcon, {
   SQUARE_ICON_COLORS,
   SquareIconProps,
-} from '~/shared/components/SquareIcon/SquareIcon.types';
+} from '~/shared/components/SquareIcon';
 import { ReactComponent as SVGIcon } from './fixtures/bicycle.svg';
 
 export default {
-  title: 'New Title',
-  args: {
-    alt: 'My label',
-    icon: SVGIcon,
-    color: SQUARE_ICON_COLORS.PRIMARY,
-  },
+  title: 'Components/SquareIcon',
 } satisfies StoryDefault<SquareIconProps>;
+
+export const Primary: Story = () => (
+  <SquareIcon
+    alt="My label"
+    icon={SVGIcon}
+    color={SQUARE_ICON_COLORS.PRIMARY}
+  />
+);
+
+export const Red: Story = () => (
+  <SquareIcon alt="My label" icon={SVGIcon} color={SQUARE_ICON_COLORS.RED} />
+);
+
+export const Blue: Story = () => (
+  <SquareIcon alt="My label" icon={SVGIcon} color={SQUARE_ICON_COLORS.BLUE} />
+);
+
+export const Yellow: Story = () => (
+  <SquareIcon alt="My label" icon={SVGIcon} color={SQUARE_ICON_COLORS.YELLOW} />
+);
+
+export const Pink: Story = () => (
+  <SquareIcon alt="My label" icon={SVGIcon} color={SQUARE_ICON_COLORS.PINK} />
+);
